ae948347696054fd77fed95306dc1ceb6d093f14
Tim Huitt <timhuitt@gmail.com>
2024-03-25 00:10:42 -0600

    update footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,11 +4,11 @@ const Footer: React.FC = () => {
   
   return (
     <footer className="fixed fixed bottom-0 flex justify-center w-full h-12 items-center px-4 z-50">
-      <div className="flex justify-between space-x-20 max-w-lg">
-        <button>Tattoos</button>
-        <button>Illustration</button>
-        <button>Painting</button>
-        <button>Other</button>
+      <div className="flex justify-between space-x-20 max-w-">
+        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Tattoos</button>
+        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Illustration</button>
+        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Painting</button>
+        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Other</button>
       </div>
     </footer>
   )
