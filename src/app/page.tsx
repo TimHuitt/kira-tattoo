dcb026dc92da9d5fc0f9b22e2e34ed57796c52f8
Tim Huitt <timhuitt@gmail.com>
2024-03-25 23:05:38 -0600

    update layout, add scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative flex min-h-full items-center mx-auto z-10">
-      <div className="flex justify-center pt-12 w-11/12 max-w-lg min-h-screen text-white mx-auto bg-zinc-800 shadow-xl overflow-auto">
-        Hello!
+    <main className="fixed w-full h-full flex items-center mx-auto mt-10 overflow-hidden z-10">
+      <div className="pt-12 w-5/6 max-w-3xl h-full text-white mx-auto pb-40 p-6 bg-zinc-800 shadow-lg overflow-auto">
+        <h1>Hello!</h1>
+        <p>
+        Sapien, pulvinar nostra integer sociosqu dignissim ultrices tincidunt ad penatibus sed! Mus porttitor felis et; pellentesque diam. Blandit quisque, posuere pretium vivamus. Metus urna mattis taciti pharetra nascetur maecenas. Justo placerat lacus ipsum. Ad sed ante eu eleifend massa aenean aptent curae; justo placerat. Proin sollicitudin dui dictum parturient fusce felis ad felis montes. Ante arcu porta laoreet; mus porttitor eros cras arcu vestibulum imperdiet. Tempor primis molestie nisl litora rhoncus. Mus commodo taciti fames. Tortor tincidunt nascetur ac netus curabitur nisl volutpat varius.
+        </p><p>
+        Fusce orci nostra suspendisse velit taciti iaculis congue aenean mattis. Nulla sollicitudin neque tristique nostra dictumst? Lobortis curabitur dui molestie magnis vehicula ultrices dictumst. Facilisis curabitur montes pulvinar gravida eu tristique vel parturient, duis ligula. Facilisi porttitor dignissim nisi massa integer. Interdum per porta, quisque dictumst nec amet nostra nunc! Nisl, nisi etiam lacinia iaculis dictumst iaculis facilisi! Tempor id urna condimentum id inceptos praesent. Ultrices vulputate phasellus tincidunt vitae lorem ac nullam eleifend iaculis penatibus. Vivamus parturient ante enim amet risus, nec quam vehicula! Consequat.
+        </p><p>
+        Placerat ipsum sapien duis duis nisl. Nisl ligula at iaculis. Sed taciti cum est scelerisque bibendum nec et leo himenaeos taciti. Bibendum ultrices diam commodo lectus velit per in amet ut aliquam. Est tempor elementum est curabitur fames ultricies leo. Netus ligula tempus proin netus non primis ad primis inceptos lectus inceptos lacus. Magnis a placerat imperdiet.
+        </p><p>
+        Mollis penatibus ligula dignissim pellentesque! Dictum, cum posuere accumsan nisl sollicitudin parturient pretium. Tortor aptent ullamcorper hac. Eu quam vel egestas senectus semper accumsan enim tortor porta faucibus massa dui. Cum est est bibendum praesent nunc tempor. Quis mauris quam class amet proin. Fames lectus taciti felis elementum ante sollicitudin ipsum pellentesque. Habitasse dui volutpat hendrerit nostra turpis commodo mauris. Netus porta, et odio hac class fusce. Dapibus fringilla mattis nisl ornare pulvinar viverra.
+        </p><p>
+        Mauris fringilla duis ultrices semper hac. Mus orci cum volutpat porta class tristique sociosqu venenatis scelerisque. Nisi inceptos semper lacinia malesuada ligula. Inceptos aliquam posuere habitasse pellentesque facilisi feugiat at praesent dignissim hendrerit dolor. Per diam rutrum praesent orci netus magnis lobortis molestie cubilia ultricies molestie. Nisi consectetur habitant semper augue donec justo turpis curabitur vitae taciti potenti pretium! Arcu montes, enim erat purus nostra. Dictumst quam conubia nullam.
+        </p><p>
+        Suspendisse dui sit lobortis orci parturient ut curabitur ornare aptent duis. Praesent velit sociis posuere velit rutrum ultricies quisque quis ut massa cras per. Velit eleifend nec sagittis nam arcu elementum senectus! Id convallis blandit consequat vestibulum torquent est auctor faucibus primis id. Ad neque, convallis consequat parturient per torquent habitant dui metus! Cras parturient faucibus adipiscing ligula.
+        </p><p>
+        Vivamus consequat, molestie class maecenas fermentum varius et erat tellus faucibus suscipit aliquam. Facilisis ultrices vehicula gravida praesent nullam dapibus suspendisse cursus nibh! Urna rutrum quam purus lorem elementum scelerisque ullamcorper ultrices eu ligula. Suspendisse augue molestie taciti nisl vehicula sociis ridiculus magna per dictumst. Nisi commodo lorem euismod habitant libero arcu primis eu tincidunt rhoncus nulla integer! Est nascetur viverra neque metus nunc cubilia primis per maecenas. Cum dolor potenti egestas augue sem volutpat! Interdum nascetur lacinia phasellus magna! Lobortis feugiat sed quam primis curae; fusce sodales hendrerit elit blandit orci? Ut rutrum class.
+        </p><p>
+        Conubia sagittis tristique tortor sodales nam convallis molestie. Urna sed, tempus augue laoreet ultrices netus. Justo neque fringilla varius. Condimentum lacinia vulputate duis rhoncus egestas porta nam suspendisse feugiat ridiculus phasellus! Donec, penatibus viverra vehicula a sociis sapien eu per. Dictumst amet consequat congue ante nostra suscipit senectus quisque convallis libero sem. Condimentum ante lobortis hac arcu consequat elementum sollicitudin nisl taciti feugiat. Ornare nascetur in risus ultrices aptent porta cras? Magnis ut donec nec. Malesuada sit ante quisque fermentum laoreet. Egestas aliquet semper est.
+        </p><p>
+        Nibh dolor porta a sagittis ante aliquet sed. Consectetur nam convallis curae; nibh nam mi suscipit non at magnis cras dis. Varius aliquam elit nulla laoreet conubia nisi interdum curae;. Lobortis mollis suspendisse facilisi sit ad litora nibh. Gravida adipiscing non a fames ac pharetra fringilla ridiculus in. Aptent ad ultrices sagittis sapien pharetra pellentesque lorem. Sociosqu porta augue litora velit sit ante lorem urna justo montes purus. Purus blandit nullam fames. Quis libero, dis viverra blandit platea praesent mauris. Suscipit tempus neque dictumst.
+        </p><p>
+        Nullam posuere lacinia nullam. Ornare vestibulum venenatis egestas cubilia curae;. Purus habitasse vel mi cras ultrices amet massa donec libero condimentum. Velit ut porttitor lectus felis dapibus ornare iaculis praesent per. Nam aliquam dui faucibus. Consectetur molestie nibh malesuada quisque vivamus sagittis bibendum penatibus non aenean. Amet ridiculus natoque maecenas neque proin. Himenaeos magnis iaculis purus per risus mattis adipiscing orci. Pharetra consequat venenatis elementum sociis donec massa integer.
+        </p>
       </div>
     </main>
   );
--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,10 @@ import styles from './Footer.module.css'
 const Footer: React.FC = () => {
   
   return (
-    <footer className="fixed bottom-0 flex justify-center items-end w-full h-20 px-4 z-50">
-      <div className="flex justify-between items-end space-x-20 max-w-2xl h-full">
+    <footer className="fixed bottom-0 flex justify-center items-end w-screen h-20 z-50">
+      <div className="flex justify-between items-end w-full max-w-4xl h-full px-4">
         <button className='tab tab-hover'>Tattoos</button>
-        <button className='tab tab-hover'>Illustration</button>
+        <button className='tab tab-hover tab-selected'>Illustration</button>
         <button className='tab tab-hover'>Painting</button>
         <button className='tab tab-hover'>Other</button>
       </div>
