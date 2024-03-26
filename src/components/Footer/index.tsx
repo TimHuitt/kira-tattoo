fdbb280374130508910c019abbbd719031712421
Tim Huitt <timhuitt@gmail.com>
2024-03-25 20:51:57 -0600

    add hover effects

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,12 +3,12 @@ import styles from './Footer.module.css'
 const Footer: React.FC = () => {
   
   return (
-    <footer className="fixed fixed bottom-0 flex justify-center w-full h-12 items-center px-4 z-50">
-      <div className="flex justify-between space-x-20 max-w-">
-        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Tattoos</button>
-        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Illustration</button>
-        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Painting</button>
-        <button className='pt-4 pb-2 px-6 border-4 border-pink-900 rounded-xl rounded-b-none bg-zinc-900 bg-opacity-75'>Other</button>
+    <footer className="fixed bottom-0 flex justify-center items-end w-full h-20 px-4 z-50">
+      <div className="flex justify-between items-end space-x-20 max-w-2xl h-full">
+        <button className='tab tab-hover'>Tattoos</button>
+        <button className='tab tab-hover'>Illustration</button>
+        <button className='tab tab-hover'>Painting</button>
+        <button className='tab tab-hover'>Other</button>
       </div>
     </footer>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,14 +3,14 @@ import styles from './Header.module.css'
 const Header: React.FC = () => {
   
   return (
-    <header className="relative flex h-12 justify-between items-center bg-zinc-900 shadow-lg shadow-violet-500/50 z-50">
+    <header className="relative flex h-12 justify-between items-center bg-zinc-900 shadow-md shadow-violet-500/50 z-50">
       <div className="flex items-center h-full px-4 bg-purple-600">
         <h1 className="text-4xl">Kira</h1>
       </div>
       <div className="flex space-x-4 px-4">
-        <button>Portfolio</button>
-        <button>Booking</button>
-        <button>Contact</button>
+        <button className='btn btn-hover'>Portfolio</button>
+        <button className='btn btn-hover'>Booking</button>
+        <button className='btn btn-hover'>Contact</button>
       </div>
     </header>
   )
