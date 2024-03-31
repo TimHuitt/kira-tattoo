a58aba56207c07d96224a4370f9245bfa6ac2a31
Tim Huitt <timhuitt@gmail.com>
2024-03-31 02:03:25 -0600

    update footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,14 +1,24 @@
 'use client'
 
+import Link from 'next/link'
+
 const Footer: React.FC = () => {
   
   return (
     <footer className="fixed bottom-0 left-0 flex justify-center items-end w-screen max-w-screen h-20 z-50">
       <div className="flex justify-between items-end w-full max-w-4xl h-full px-4">
-        <button className='tab tab-hover'>Tattoos</button>
-        <button className='tab tab-hover tab-selected'>Illustration</button>
-        <button className='tab tab-hover'>Painting</button>
-        <button className='tab tab-hover'>Other</button>
+        <Link href='/portfolio/tattoo' className='tab tab-hover'>
+          <button >Tattoos</button>
+        </Link>
+        <Link href='/portfolio/illustration' className='tab tab-hover'>
+          <button >Illustration</button>
+        </Link>
+        <Link href='/portfolio/painting' className='tab tab-hover'>
+          <button >Painting</button>
+        </Link>
+        <Link href='/portfolio/other' className='tab tab-hover'>
+          <button >Other</button>
+        </Link>
       </div>
     </footer>
   )
