ae8216e43fd5a7cac1390c6a57fd620a7050b605
Tim Huitt <timhuitt@gmail.com>
2024-03-30 22:02:05 -0600

    update layout, add home header, add divider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,39 +2,39 @@
 
 import { useState, useEffect } from 'react'
 import Gallery from '../components/Gallery'
+import Divider from '../components/Divider'
 import Image from 'next/image'
 
 export default function Home() {
   const [ images, setImages ] = useState<string[]>(([]))
   
   useEffect(() => {
-    setImages(['/images/1.webp', '/images/2.webp', '/images/6.webp', '/images/7.webp', '/images/8.webp', '/images/9.jpg', '/images/10.webp', '/images/11.jpg', '/images/12.jpg', '/images/13.webp', '/images/14.webp', '/images/15.webp', '/images/16.webp', '/images/17.jpg', ])
+    setImages(['/images/2.webp', '/images/6.webp', '/images/7.webp', '/images/8.webp', '/images/9.jpg', '/images/10.webp', '/images/11.jpg', '/images/12.jpg', '/images/13.webp', '/images/14.webp', '/images/15.webp', '/images/16.webp', '/images/17.jpg', ])
   },[])
 
   return (
     <div className="relative z-30">
-      <div className='w-5/6 h-[50vmin] max-h-60'>
-        <div className='w-[50px] h-[50px] rounded-full overflow-hidden'>
+      <div className='w-5/6 max-h-60'>
+        <div className='w-[75px] h-[75px] rounded-full overflow-hidden'>
           <Image
             src="/images/1.webp"
             alt=""
             objectFit="contain"
-            width={50}
-            height={50}
-            sizes="(max-width: 768px) 100vw, 33vw"
+            width={75}
+            height={75}
           />
         </div>
         <br />
         <h1 className="text-4xl text-start">Hi, I&apos;m Kira!</h1>
-        <br />
-        <small>Learn more about me, browse my works, set appointments.</small>
       </div>
       <div className='max-w-full overflow-hidden'>
         <Gallery images={ images } />
       </div>
+      <Divider />
       <p>
       Sapien, pulvinar nostra integer sociosqu dignissim ultrices tincidunt ad penatibus sed! Mus porttitor felis et; pellentesque diam. Blandit quisque, posuere pretium vivamus. Metus urna mattis taciti pharetra nascetur maecenas. Justo placerat lacus ipsum. Ad sed ante eu eleifend massa aenean aptent curae; justo placerat. Proin sollicitudin dui dictum parturient fusce felis ad felis montes. Ante arcu porta laoreet; mus porttitor eros cras arcu vestibulum imperdiet. Tempor primis molestie nisl litora rhoncus. Mus commodo taciti fames. Tortor tincidunt nascetur ac netus curabitur nisl volutpat varius.
       </p>
+      <Divider />
       <p>
       Fusce orci nostra suspendisse velit taciti iaculis congue aenean mattis. Nulla sollicitudin neque tristique nostra dictumst? Lobortis curabitur dui molestie magnis vehicula ultrices dictumst. Facilisis curabitur montes pulvinar gravida eu tristique vel parturient, duis ligula. Facilisi porttitor dignissim nisi massa integer. Interdum per porta, quisque dictumst nec amet nostra nunc! Nisl, nisi etiam lacinia iaculis dictumst iaculis facilisi! Tempor id urna condimentum id inceptos praesent. Ultrices vulputate phasellus tincidunt vitae lorem ac nullam eleifend iaculis penatibus. Vivamus parturient ante enim amet risus, nec quam vehicula! Consequat.
       </p>
---
+++ src/components/Divider/index.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image'
+
+const Divider: React.FC = () => {
+  return (
+    <div className='w-full h-10 overflow-hidden'>
+      <Image
+          src="divider.svg"
+          alt="divider"
+          objectFit="contain"
+          height={10}
+          width={400}
+          sizes="(max-width: 768px) 100vw, 33vw"
+        />
+    </div>
+  )
+}
+
+export default Divider
