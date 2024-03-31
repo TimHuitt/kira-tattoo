21033c966984f49a132975b97740962c90674501
Tim Huitt <timhuitt@gmail.com>
2024-03-31 14:02:42 -0600

    get cloudinary image list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,30 +7,29 @@ import Divider from '../components/Divider'
 import Modal from '../components/Modal'
 import Image from 'next/image'
 
-export default function Home() {  
-  const [ images2, setImages2 ] = useState<string[]>(([]))
+export default function Home() { 
   const [ images, setImages ] = useState<string[]>(([]))
   const { showModal, currentImage } = useModalContext()
 
+  // useEffect(() => {
+  //   setImages(['/images/2.webp', '/images/6.webp', '/images/7.webp', '/images/8.webp', '/images/9.jpg', '/images/10.webp', '/images/11.jpg', '/images/12.jpg', '/images/13.webp', '/images/14.webp', '/images/15.webp', '/images/16.webp', '/images/17.jpg', ])
+  // },[])
+
   useEffect(() => {
-    setImages2(["/stars2.png", "/stars.png", "/corner.svg", "/stars2.png", "/stars.png", "/corner.svg"])
-    
     async function fetchImages() {
       try {
-        // Fetching data from your internal API route
-        const res = await fetch('/api/route');
-        const data = await res.json();
-        if (data && data.resources) {
-          console.log(data.resources)
-          // setImages(data.resources.map((resource: { url: any }) => resource.url));
+        const res = await fetch('/api/route')
+        const data = await res.json()
+        if (data) {
+          const finalData = data.data.map((resource: { public_id: string }) => resource.public_id)
+          console.log(finalData)
+          setImages(finalData);
         }
       } catch (e) {
-        console.error("Fetch Error:", e);
+        console.error("Fetch Error:", e)
       }
     }
-
-    fetchImages();
-
+    fetchImages()
   },[])
 
 
@@ -56,6 +55,12 @@ export default function Home() {
           <Gallery images={ images } />
         </div>
         <Divider />
+        {images.map((i) => (
+
+          <div key={i} className="">
+            { i }
+          </div>
+        ))}
         <p>
         Sapien, pulvinar nostra integer sociosqu dignissim ultrices tincidunt ad penatibus sed! Mus porttitor felis et; pellentesque diam. Blandit quisque, posuere pretium vivamus. Metus urna mattis taciti pharetra nascetur maecenas. Justo placerat lacus ipsum. Ad sed ante eu eleifend massa aenean aptent curae; justo placerat. Proin sollicitudin dui dictum parturient fusce felis ad felis montes. Ante arcu porta laoreet; mus porttitor eros cras arcu vestibulum imperdiet. Tempor primis molestie nisl litora rhoncus. Mus commodo taciti fames. Tortor tincidunt nascetur ac netus curabitur nisl volutpat varius.
         </p>
---
+++ src/pages/api/route.ts
@@ -0,0 +1,39 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      await handleGet(req, res);
+      break;
+    case 'POST':
+      // await handlePost(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
+
+async function handleGet(req: NextApiRequest, res: NextApiResponse) {
+  const url = `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/resources/image`
+  
+  try {
+    const resData = await fetch(url, {
+      headers: {
+        Authorization: `Basic ${Buffer.from(process.env.CLOUDINARY_API_KEY + ':' + process.env.CLOUDINARY_API_SECRET).toString('base64')}`
+      },
+    })
+
+    if (!resData.ok) {
+      return res.status(resData.status).json({ error: 'Fetch Failure' });
+    }
+
+    const data = await resData.json()
+    const resource = data.resources
+    return res.status(200).json({ data: resource })
+    
+  } catch (err: any) {
+    console.error("Fetch Error:", err)
+    return res.status(500).json({ error: 'Error', message: err.message })
+  }
+}
