a8eb42b8adbd21a580c35f847410b5604e83d018
Tim Huitt <timhuitt@gmail.com>
2024-04-02 01:36:36 -0600

    update panel layout and sizing, add panel display toggle

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -8,13 +8,15 @@ import Panel from '../../components/Panel'
 export default function Admin() {  
   return (
     <div className="relative h-full z-30">
-      <div className='flex justify-center items-center w-5/6 max-h-60 min-h-40 md:min-h-40 md:min-h-56'>
-        <h1 className="text-4xl text-start">Admin Panel</h1>
+      <div className='flex justify-start items-center w-5/6 max-h-60 min-h-40 md:min-h-40 md:min-h-56'>
+        <h1 className="text-xl md:text-4xl text-start">Admin Panel</h1>
       </div>
-      
+      <div className='flex justify-center items-center py-4'>
+        <h1 className="text-xl md:text-4xl text-start">Add New</h1>
+      </div>
       <Panel
-        header={'Upload Image'}
-        description={'Select an image, select category, add details.'}
+        header={'Image'}
+        description={'Upload an image'}
         image={true}
         categories={[
           'featured',
@@ -27,8 +29,43 @@ export default function Admin() {
       />
 
       <Panel
-        header={'Create New Post'}
-        description={'Select an image, select category, add details.'}
+        header={'Post'}
+        description={'Create a new post'}
+        image={false}
+        categories={[
+          'updates', 
+          'events'
+        ]}
+        inputType={'post'}
+      />
+
+      <div className='flex justify-center items-center py-4'>
+        <h1 className="text-xl md:text-4xl text-start">Manage</h1>
+      </div>
+
+      <Panel
+        header={'Images'}
+        description={'Browse, Edit, and Remove'}
+        image={false}
+        categories={[
+          'updates', 
+          'events'
+        ]}
+        inputType={'post'}
+      />
+      <Panel
+        header={'Posts'}
+        description={'Browse, Edit, and Remove'}
+        image={false}
+        categories={[
+          'updates', 
+          'events'
+        ]}
+        inputType={'post'}
+      />
+      <Panel
+        header={'Users'}
+        description={'Manage authorized admins'}
         image={false}
         categories={[
           'updates', 
--- src/components/Panel/index.tsx
+++ src/components/Panel/index.tsx
@@ -14,6 +14,7 @@ type PanelProp = {
 
 const Panel: React.FC<PanelProp> = (props) => {
   const [ imgCategory, setImgCategory ] = useState<string>('')
+  const [ hidden, setHidden ] = useState<string>('hidden')
 
   const cld = new Cloudinary({
     cloud: {
@@ -39,45 +40,53 @@ const Panel: React.FC<PanelProp> = (props) => {
     file && reader.readAsDataURL(file)
   }
 
+  const handleClick = () => {
+    setHidden(prev => prev === 'hidden' ? '' : 'hidden')
+  }
+
   return (
-    <div className="w-full p-4 pt-6 mb-6 rounded-xl border border-1 border-slate-700 bg-slate-800">
-        <h2 className="text-xl py-2">{props.header}</h2>
-        <h4 className="text-base pl-4">{props.description}</h4>
-        <div className="w-full flex justify-center my-4">
-          <div className="w-5/6 h-1 rounded-xl border border-1 border-slate-500" />
+    <div className='w-full p-4 pt-2 mb-6 rounded-xl border border-1 border-slate-700 bg-slate-800 hover:bg-slate-700'>
+        <div className='w-full cursor-pointer' onClick={handleClick}>
+          <h2 className="text-base md:text-xl py-2">{props.header}</h2>
+          <h4 className="text-xs md:text-base text-end pl-4">{props.description}</h4>
         </div>
-          { props.image && (
-            <div className="w-full flex flex-col items-center justify-center">
-              <label htmlFor="file-upload" className="flex flex-col items-center justify-center cursor-pointer">
-                <div className="h-20 flex justify-center m-4 border border-2 border-slate-500 rounded-lg hover:bg-slate-700 cursor-pointer">
-                  <Image 
-                    src='/images/image.svg'
-                    height={100}
-                    width={100}
-                    alt={''} 
-                  />
-                </div>
-                <input id="file-upload" type="file" style={{ display: 'none' }} onChange={handleFileSelect} />
-              </label>
-            </div>
-          )}
-        <div className='flex justify-center items-center w-full py-4'>
-          <h2 className="text-xl mr-4">Category</h2>
-          <select value={imgCategory} className='p-2 rounded-lg bg-slate-900' onChange={handleChange}>
-            {props.categories.map((item, index) => {
-              const itemName = item.charAt(0).toUpperCase() + item.slice(1)
-              return (
-                <option key={`${item}-${index}`} value={item}>{itemName}</option>
-              )
-            })}
-          </select>
-        </div>
-        <div className='flex justify-center items-center w-full py-4'>
-          <h2 className="text-xl mr-4">Caption</h2>
-          <input type="text" placeholder="Something about this image" className='p-2 w-3/5 rounded-lg bg-slate-900' />
-        </div>
-        <div className='flex justify-center items-center w-full py-4'>
-          <button className="border border-2 rounded-lg p-2 hover:bg-slate-600">Add New Image</button>
+        <div className={hidden}>
+          <div className="w-full flex justify-center my-4">
+            <div className="w-5/6 h-1 rounded-xl border border-1 border-slate-500" />
+          </div>
+            { props.image && (
+              <div className="w-full flex flex-col items-center justify-center">
+                <label htmlFor="file-upload" className="flex flex-col items-center justify-center cursor-pointer">
+                  <div className="h-20 flex justify-center m-4 rounded-lg hover:bg-slate-500 cursor-pointer">
+                    <Image 
+                      src='/images/image.svg'
+                      height={100}
+                      width={100}
+                      alt={''} 
+                    />
+                  </div>
+                  <input id="file-upload" type="file" style={{ display: 'none' }} onChange={handleFileSelect} />
+                </label>
+              </div>
+            )}
+          <div className='flex justify-center items-center w-full py-4'>
+            <h2 className="text-xl mr-4">Category</h2>
+            <select value={imgCategory} className='p-2 rounded-lg bg-slate-900 hover:bg-slate-500' onChange={handleChange}>
+              {props.categories.map((item, index) => {
+                const itemName = item.charAt(0).toUpperCase() + item.slice(1)
+                return (
+                  <option key={`${item}-${index}`} value={item}>{itemName}</option>
+                )
+              })}
+            </select>
+          </div>
+          <div className='flex justify-center items-center w-full py-4'>
+            <h2 className="text-xl mr-4">Caption</h2>
+            <input type="text" placeholder="Something about this image" className='p-2 w-3/5 rounded-lg bg-slate-900 hover:bg-slate-500' />
+          </div>
+          <div className='flex justify-center items-center w-full py-4'>
+            <button className="border border-2 rounded-lg p-2 hover:bg-slate-500">Add New Image</button>
+          </div>
         </div>
       </div>
   )
