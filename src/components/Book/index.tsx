794160438319e0ef41046410ad69d866e3ba0984
Tim Huitt <timhuitt@gmail.com>
2024-04-10 12:44:37 -0600

    update booking window layout

--- src/components/Book/index.tsx
+++ src/components/Book/index.tsx
@@ -16,20 +16,20 @@ const Book: React.FC<dates> = ({date, handleClick}) => {
         onClick={handleClick}
       >
         <div 
-          className="relative w-[90%] md:w-3/5 max-h-3/5 flex flex-col items-center bg-slate-800 border border-2 border-slate-500 rounded-lg overflow-y-auto z-50"
+          className="relative w-[90%] md:w-3/5 h-5/6 max-h-5/6 flex flex-col items-center bg-slate-800 border border-2 border-slate-500 rounded-lg overflow-y-auto z-50"
           onClick={(e) => e.stopPropagation()} 
         >
           <div className='absolute top-2 right-2 px-2 bg-fuchsia-900 rounded-full cursor-pointer' onClick={handleClick}>x</div>
-          <h1 className='py-4'>Book Some Time!</h1>
-          <div className="w-5/6 h-fill flex flex-col items-center text-center">
+          <h1 className='py-8'>Book Some Time!</h1>
+          <div className="relative w-5/6 h-fill flex flex-col items-center text-center">
 
-            <p className="label">Name</p>
-            <input type="text" className="input"></input>
+            <p className="book-label">Name</p>
+            <input type="text" className="book-input"></input>
 
-            <p className="label">Email</p>
-            <input type="text" className="input"></input>
+            <p className="book-label">Email</p>
+            <input type="text" className="book-input"></input>
 
-            <p className="label">Comments</p>
+            <p className="book-label">Comments</p>
             <textarea className="input text-area"></textarea>
             
             <div className="w-full p-4 my-4">
