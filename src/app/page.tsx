a1c38751805a26382eee270f7c8e69308aaf6452
Tim Huitt <timhuitt@gmail.com>
2024-04-11 20:18:30 -0600

    update profile image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -94,15 +94,15 @@ export default function Home() {
       <div className='h-auto w-[90%] max-w-5xl text-white mx-auto  p-4 pt-10 md:p-6 lg:p-8 rounded bg-slate-800 shadow-xl shadow-slate-900 overflow-x-hidden'>
         <div className="relative">
           <div className='w-5/6 max-h-60 min-h-20 md:min-h-40 md:min-h-56'>
-            <div className='w-[75px] h-[75px] mb-10 rounded-full overflow-hidden'>
+            <div className='w-[100px] h-[100px] mb-6 rounded-full overflow-hidden'>
               <Image
                 src="/images/1.webp"
                 alt="Profile Image"
                 style={{
                   objectFit: 'contain',
                 }}
-                width={75}
-                height={75}
+                width={100}
+                height={100}
               />
             </div>
             <h1 className="text-4xl text-start">Hi, I&apos;m Kira!</h1>
